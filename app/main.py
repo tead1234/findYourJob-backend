f67b0830262f749ea0ef2588a945f829020adb9e
msko <tead1234@gmail.com>
2024-10-09 18:21:33 +0900

    기능: GJ-35 middleware 설정

--- app/main.py
+++ app/main.py
@@ -2,11 +2,20 @@ import os
 import sys
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from app.controllers import user_face_controller
 from fastapi.staticfiles import StaticFiles
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Include the router from the controller module
 app.include_router(user_face_controller.router, prefix='/face_api')
@@ -23,4 +32,4 @@ async def root():
     return {"message": "Hello World"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
