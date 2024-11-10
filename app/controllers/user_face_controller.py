4f54534e9ee06c99faf7b9a9f6bf7caa7ec42936
tead1234 <tead1234@gmail.com>
2024-11-10 20:40:03 +0900

    기능: job1 체크

--- app/controllers/user_face_controller.py
+++ app/controllers/user_face_controller.py
@@ -48,6 +48,7 @@ async def upload_image(file: UploadFile = File(...),
                 job2=prediction['predicted_job2_translated'],
                 job3=prediction['predicted_job3_translated']
             )
+            print(job1, job2, job3)
             face_image_repo = face_image_repository()
             result_id = await face_image_repo.save_face_image(face_image=face_entity)
             picture_number = await face_image_repo.get_total_records()
